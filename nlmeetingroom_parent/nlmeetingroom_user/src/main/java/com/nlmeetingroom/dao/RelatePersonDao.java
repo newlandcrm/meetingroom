a80cfbd0cdc75ccbdbf5c28d45ccf83c7e7b489c
zhb <1095828579@qq.com>
2019-08-20 14:36:11 +0800

    解决    添加相关人员重复的情况

--- nlmeetingroom_parent/nlmeetingroom_user/src/main/java/com/nlmeetingroom/dao/RelatePersonDao.java
+++ nlmeetingroom_parent/nlmeetingroom_user/src/main/java/com/nlmeetingroom/dao/RelatePersonDao.java
@@ -4,11 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.nlmeetingroom.pojo.RelatePerson;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 /**
  * 数据访问接口
  * @author Administrator
  *
  */
 public interface RelatePersonDao extends JpaRepository<RelatePerson,String>,JpaSpecificationExecutor<RelatePerson>{
-	
+
+    public void deleteByReserveid(String reserveid);
 }
